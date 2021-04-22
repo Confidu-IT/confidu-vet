10f7840d32e66cdeb35e0307c2f1c9e758bd3fff
mkalden <post@matthias-kalden.de>
2021-04-22 13:08:43 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDRiHrFLubf7mqQoykAl216RPOh4lnzGP0',
+    authDomain: 'confidu-app.firebaseapp.com',
+    databaseURL: 'https://confidu-app.firebaseio.com',
+    projectId: 'confidu-app',
+    storageBucket: 'confidu-app.appspot.com',
+    messagingSenderId: '944987547095',
+    appId: '1:944987547095:web:73f32524698d702a0f1792',
+    vapidKey: 'BMqCmorVeDKwwwQzRVEVSXU5UVnWkzZfB1kAcMr6acpoIhjJ3Q3S8c-VC_PDZtr7VUj_PW6Mev4ucRs6t50YCe4'
+  },
+  logo: '../assets/icons/logo_confid.svg',
+  baseUrl: 'https://staging-backend-dot-confidu-app.ey.r.appspot.com',
+  homeButton: '../../assets/icons/home-button.svg',
+  iconPath: '../assets/icons',
+  shopware: {
+    accessKey: 'SWSCULBAMMXOVW5VSKPZRWY0QW'
+  },
+  storefrontUrl: 'https://confidu-app.firebaseapp.com'
 };
