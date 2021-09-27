7024d22291c64fda51dba78a4ff76a451c1e86cd
mkalden <post@matthias-kalden.de>
2021-09-27 17:10:49 +0200

    [update] config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,10 +8,11 @@ export const environment = {
     storageBucket: 'confidu-app.appspot.com',
     messagingSenderId: '944987547095',
     appId: '1:944987547095:web:73f32524698d702a0f1792',
+    measurementId: 'G-XZB7VR6BZ3',
     vapidKey: 'BMqCmorVeDKwwwQzRVEVSXU5UVnWkzZfB1kAcMr6acpoIhjJ3Q3S8c-VC_PDZtr7VUj_PW6Mev4ucRs6t50YCe4'
   },
   logo: '../assets/icons/logo_confid.svg',
-  baseUrl: 'https://confidu-proxy-dot-confidu-app.appspot.com',
+  baseUrl: 'https://confidu-proxy-develop-dot-confidu-app.appspot.com',
   homeButton: '../../assets/icons/home-button.svg',
   iconPath: '../assets/icons',
   shopware: {
---
+++ src/environments/environment.test.app.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDq1kB201k3JwAQn16Cc87XUeiZkPOj4t8',
+    authDomain: 'confidu-app-test.firebaseapp.com',
+    databaseURL: 'https://confidu-app-test-default-rtdb.firebaseio.com',
+    projectId: 'confidu-app-test',
+    storageBucket: 'confidu-app-test.appspot.com',
+    messagingSenderId: '862692374801',
+    appId: '1:862692374801:web:6a0a2ec0ef4af53cfbb4b0',
+    measurementId: 'G-R2NPP4R9X8'
+  },
+  logo: '../assets/icons/logo_confid.svg',
+  baseUrl: 'https://confidu-app-test-dot-confidu-app.appspot.com',
+  homeButton: '../../assets/icons/home-button.svg',
+  iconPath: '../assets/icons',
+  shopware: {
+    accessKey: 'SWSCULBAMMXOVW5VSKPZRWY0QW'
+  },
+  storefrontUrl: 'https://confidu-app.firebaseapp.com'
+};
