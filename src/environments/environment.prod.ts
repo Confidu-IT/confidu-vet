d458be14bb47a61de39b1ec0fe13f4c3459434c0
mkalden <post@matthias-kalden.de>
2022-01-10 12:04:01 +0100

    [update] disease key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,7 +12,7 @@ export const environment = {
     vapidKey: 'BMqCmorVeDKwwwQzRVEVSXU5UVnWkzZfB1kAcMr6acpoIhjJ3Q3S8c-VC_PDZtr7VUj_PW6Mev4ucRs6t50YCe4'
   },
   logo: '../assets/icons/logo_confid.svg',
-  baseUrl: 'https://confidu-proxy-develop-dot-confidu-app.appspot.com',
+  baseUrl: 'https://confidu-proxy-dot-confidu-app.appspot.com',
   homeButton: '../../assets/icons/home-button.svg',
   iconPath: '../assets/icons',
   shopware: {
